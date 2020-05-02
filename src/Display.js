9571cf4ca7dfb609e1c29c162de5db4d531c6a66
RafCas88 <raffaelecastaldo88@libero.it>
2020-05-02 18:24:32 +0100

    working on highlited values

--- src/Display.js
+++ src/Display.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import Popup from "reactjs-popup";
 
-function Display ({ title, items = [], multiSelect = false }) {
-// const [selection, setSelection] = useState([]);
+function Display ({ items }) {
+
+  function handleOnClick(item) {}
 
 
-function handleOnClick(item) {}
 
-return (
-  <div className='injury-box'>
-  <Popup trigger={<button> MECHANISM</button>} position="bottom center">
-  <div>
-  {items.map(item => (
-             <div className="dd-list-item" key={item.id}>
-              <button type="button" onClick={() => handleOnClick(item)}>
-                <span>{item.value}</span>
-              </button>
-</div>
-          ))}
+  return (
+    <div className='injury-box'>
+    <Popup trigger={<button> MECHANISM</button>} position="bottom center">
+    <div>
+    {items.map(item => (
+      <button key={item.id} type="button" onClick={() => handleOnClick(item)}>
+      <span>{item.value}</span>
+      </button>
 
-  </div>
-  </Popup>
-  </div>
-)
+    ))}
+
+    </div>
+    </Popup>
+    </div>
+  )
 
 }
+
+
+
 
 
 
