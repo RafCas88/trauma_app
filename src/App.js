2c49027dfd064986a7b4286eecbdef29fc407a49
RafCas88 <raffaelecastaldo88@libero.it>
2020-05-02 13:43:03 +0100

    working on highlited values

--- src/App.js
+++ src/App.js
@@ -2,10 +2,47 @@ import React, { Component } from 'react';
 import './App.css';
 import Display from './Display';
 
+const items = [
+  {
+    id: 1,
+    value: 'ABRASION',
+  },
+  {
+    id: 2,
+    value: 'BLUNT',
+  },
+  {
+    id: 2,
+    value: 'BURN',
+  },
+  {
+    id: 2,
+    value: 'FROST',
+  },
+  {
+    id: 2,
+    value: 'GUN SHOT',
+  },
+  {
+    id: 2,
+    value: 'LACERATION',
+  },
+  {
+    id: 2,
+    value: 'LTC',
+  },
+  {
+    id: 2,
+    value: 'STAB',
+  },
+];
+
 class App extends Component {
   render() {
     return (
-      <Display />
+      <div className='container'>
+      <Display items={items} />
+      </div>
     );
   }
 }
--- src/Display.js
+++ src/Display.js
@@ -1,17 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Popup from "reactjs-popup";
 
-class Display extends Component {
-  render() {
-    return (
-      <div className='injury-box'>
-      <Popup trigger={<button> MECHANISM</button>} position="bottom">
-      <div>Popup content here !!</div>
-      </Popup>
-      </div>
+function Display ({ title, items = [], multiSelect = false }) {
+// const [selection, setSelection] = useState([]);
 
-    )
-  }
+
+function handleOnClick(item) {}
+
+return (
+  <div className='injury-box'>
+  <Popup trigger={<button> MECHANISM</button>} position="bottom center">
+  <div>
+  {items.map(item => (
+             <div className="dd-list-item" key={item.id}>
+              <button type="button" onClick={() => handleOnClick(item)}>
+                <span>{item.value}</span>
+              </button>
+</div>
+          ))}
+
+  </div>
+  </Popup>
+  </div>
+)
+
 }
+
+
+
 
 export default Display;
