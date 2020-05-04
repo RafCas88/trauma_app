f550f17477eeb5aad2b75d93b4d84bb35a1e49e5
RafCas88 <raffaelecastaldo88@libero.it>
2020-05-04 12:48:14 +0100

    working on highlited values

--- src/App.js
+++ src/App.js
@@ -11,34 +11,42 @@ render() {
         {
           id: 1,
           value: 'ABRASION',
+          subtitle: 'dfds'
         },
         {
           id: 2,
           value: 'BLUNT',
+          subtitle: 'mechanism of injury'
         },
         {
           id: 2,
           value: 'BURN',
+          subtitle: 'dfdds'
         },
         {
           id: 2,
           value: 'FROST',
+          subtitle: 'dfdsdass'
         },
         {
           id: 2,
           value: 'GUN SHOT',
+          subtitle: 'dfdsfdfds'
         },
         {
           id: 2,
           value: 'LACERATION',
+          subtitle: 'dfdssasdas'
         },
         {
           id: 2,
           value: 'LTC',
+          subtitle: 'dfdsdasdas'
         },
         {
           id: 2,
           value: 'STAB',
+          subtitle: 'dfdsyrtyrty'
         },
       ]}
       />
--- src/Display.js
+++ src/Display.js
@@ -3,24 +3,35 @@ import Popup from "reactjs-popup";
 
 class Display extends React.Component{
   state = {
-    injuries: this.props.injuries
+    injuries: this.props.injuries,
+    selectedInjury: this.props.injuries
   }
 
-render() {
-  return (
-       <div className='injury-box'>
-       <Popup trigger={<button> MECHANISM</button>} position="bottom center">
-       <div>
-    {this.state.injuries.map (injury => (
-      <button key={ injury.id }>
-       {injury.value}
-      </button>
-  ))}
-  </div>
-  </Popup>
-  </div>
-)
-}
+  selectInjury = (injury) => this.setState({
+    selectedInjury: injury
+  })
+
+  render() {
+    return (
+      <>
+      <div className='select-box-selected-injury'>{this.state.selectedInjury.value }</div>
+      <div className='injury-box'>
+      <Popup trigger={<button> MECHANISM</button>} position="bottom center">
+      <div>
+      {this.state.injuries.map (injury => (
+        <button key={ injury.id }
+        onClick={() => this.selectInjury(injury)}
+        className={this.state.selectedInjury === injury ? 'selected' : ''}
+        >
+        {injury.value}
+        </button>
+      ))}
+      </div>
+      </Popup>
+      </div>
+      </>
+    )
+  }
 }
 
 
